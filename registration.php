694238fe047dcfa715165b9449a929ab74e24520
yazuu07 <nimrodthethirdpalomar@gmail.com>
2025-02-13 16:30:57 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="d1login.css">
 <head>
+
     <title>Document</title>
 </head>
 <body>
+<div class="login-container">
     <form action="action.php" method="post">
         <label>Username:</label>
         <input type="text" name="user"><br>
@@ -14,5 +17,6 @@
         <button type="submit">submit</button>
         <p>Already have an account? <a href="login.php">Login Now</a></p>
     </form>
+</div>
 </body>
 </html>
